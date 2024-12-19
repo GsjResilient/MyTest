cffd14cd25c19a11120939a62b5b3295eca865b9
GsjResilient <108513092+GsjResilient@users.noreply.github.com>
2024-12-19 17:25:08 +0800

    Update DemoApplication.java

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("你好3");
-		System.out.println("你好3");
+		System.out.println("你好4");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
