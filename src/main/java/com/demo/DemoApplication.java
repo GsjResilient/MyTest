2dfec2779d7bc1c4fbed571f7ae4bb9fa41c2f78
GsjResilient <108513092+GsjResilient@users.noreply.github.com>
2024-12-19 17:40:04 +0800

    你好03

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 		System.out.println("你好3");
         //他们俩冲突了
 
-		System.out.println("你好4");
+		System.out.println("你好3");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
